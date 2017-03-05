94b110ab396fc6e05a111a3fac35a023376e481c
Tomasz Lukawski <tomasz@lukawski.me>
2017-03-05 12:44:26 +0100

    First commit. Just basic project for electron with Vuje.js

---
+++ app/src/renderer/components/LandingPageView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <img src="./LandingPageView/assets/logo.png" alt="electron-vue">
+    <h1>GUI for</h1>
+    <img src="./LandingPageView/assets/docker.png" width="80%" height="80%">
+  </div>
+</template>
+
+<script>
+  import CurrentPage from './LandingPageView/CurrentPage';
+  import Links from './LandingPageView/Links';
+  import Versions from './LandingPageView/Versions';
+  export default {
+    components: {
+      CurrentPage,
+      Links,
+      Versions,
+    },
+    name: 'landing-page',
+  };
+</script>
+
+<style scoped>
+  img {
+    margin-top: -25px;
+    width: 450px;
+  }
+</style>
